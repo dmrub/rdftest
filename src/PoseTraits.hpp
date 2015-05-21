cc2b6ac9b59040e4d4021b54c081e1a5c00bb555
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2015-05-21 14:58:14 +0200

    Updated PoseTraits.hpp with generated code.

--- src/PoseTraits.hpp
+++ src/PoseTraits.hpp
@@ -17,7 +17,6 @@ inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Rotation &value)
 {
     using namespace Sord;
     Node _b0 = Node::blank_id(ctx.model.world());
-
     ctx.model.add_statement(_this, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:Rotation3D"));
     ctx.model.add_statement(_this, Curie(ctx.model.world(), "vom:quantityValue"), _b0);
     ctx.model.add_statement(_b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector4D"));
@@ -29,31 +28,88 @@ inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Rotation &value)
     return _this;
 }
 
-template <>
-bool fromRDF(const Context &ctx, const NodeRef _this0, ::Rotation &value)
+template<>
+inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Translation &value)
 {
     using namespace Sord;
+    Node _b0 = Node::blank_id(ctx.model.world());
+    ctx.model.add_statement(_this, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:Translation3D"));
 
-    Arvida::RDF::Triple triple = Arvida::RDF::find_triple(ctx.model, _this0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:Rotation3D"));
+    ctx.model.add_statement(_this, Curie(ctx.model.world(), "vom:quantityValue"), _b0);
 
-    // 1. Check $this
+    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector3D"));
+
+    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "maths:x"), Arvida::RDF::toRDF(ctx, value.getX()));
+
+    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "maths:y"), Arvida::RDF::toRDF(ctx, value.getY()));
+
+    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "maths:z"), Arvida::RDF::toRDF(ctx, value.getZ()));
+
+    return _this;
+}
+
+template<>
+inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Pose &value)
+{
+    using namespace Sord;
+    Node _b1 = Node::blank_id(ctx.model.world());
+    Node _b0 = Node::blank_id(ctx.model.world());
+    ctx.model.add_statement(_this, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:SpatialRelationship"));
+
+    ctx.model.add_statement(_b1, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:LeftHandedCartesianCoordinateSystem3D"));
+
+    ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:sourceCoordinateSystem"), _b1);
+
+    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:RightHandedCartesianCoordinateSystem2D"));
+
+    ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:targetCoordinateSystem"), _b0);
+    {
+        const std::string path_23 = ctx.path + "/transl";
+        Arvida::RDF::Context ctx_23(ctx, path_23);
+        Sord::URI node_23(ctx.model.world(), path_23);
+        ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:translation"), Arvida::RDF::toRDF(ctx_23, node_23, value.getTranslation()));
+    }
+    {
+        const std::string path_31 = ctx.path + "/rot";
+        Arvida::RDF::Context ctx_31(ctx, path_31);
+        Sord::URI node_31(ctx.model.world(), path_31);
+        ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:rotation"), Arvida::RDF::toRDF(ctx_31, node_31, value.getRotation()));
+    }
+    return _this;
+}
+
+template<>
+inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Device &value)
+{
+    using namespace Sord;
+    return _this;
+}
+
+template <>
+inline bool fromRDF(const Context &ctx, const NodeRef _this0, ::Rotation &value)
+{
+    using namespace Sord;
+    Arvida::RDF::Triple triple;
+
+    triple = Arvida::RDF::find_triple(ctx.model, _this0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:Rotation3D"));
+
     if (!triple.is_valid())
         return false;
     const Node _this = triple.subject;
 
-    // 2. Check blank _b0
     triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "vom:quantityValue"), Node());
     if (!triple.is_valid())
         return false;
     const Node _b0 = triple.object;
 
-    if (!Arvida::RDF::check_triple(ctx.model, _b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector4D")))
+    triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector4D"));
+    if (!triple.is_valid())
         return false;
 
-    if (!Arvida::RDF::check_triple(ctx.model, _b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Quaternion")))
+    triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Quaternion"));
+    if (!triple.is_valid())
         return false;
 
-    // maths:x
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:x"), Node());
     if (!triple.is_valid())
         return false;
@@ -64,7 +120,6 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Rotation &value)
         value.setX(tmp_value);
     }
 
-    // maths:y
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:y"), Node());
     if (!triple.is_valid())
         return false;
@@ -75,7 +130,6 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Rotation &value)
         value.setY(tmp_value);
     }
 
-    // maths:z
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:z"), Node());
     if (!triple.is_valid())
         return false;
@@ -86,7 +140,6 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Rotation &value)
         value.setZ(tmp_value);
     }
 
-    // maths:w
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:w"), Node());
     if (!triple.is_valid())
         return false;
@@ -96,49 +149,30 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Rotation &value)
             return false;
         value.setW(tmp_value);
     }
-
     return true;
 }
 
-
-template<>
-inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Translation &value)
-{
-    using namespace Sord;
-    Node _b0 = Node::blank_id(ctx.model.world());
-
-    ctx.model.add_statement(_this, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:Translation3D"));
-    ctx.model.add_statement(_this, Curie(ctx.model.world(), "vom:quantityValue"), _b0);
-    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector3D"));
-    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "maths:x"), Arvida::RDF::toRDF(ctx, value.getX()));
-    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "maths:y"), Arvida::RDF::toRDF(ctx, value.getY()));
-    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "maths:z"), Arvida::RDF::toRDF(ctx, value.getZ()));
-    return _this;
-}
-
 template <>
-bool fromRDF(const Context &ctx, const NodeRef _this0, ::Translation &value)
+inline bool fromRDF(const Context &ctx, const NodeRef _this0, ::Translation &value)
 {
     using namespace Sord;
-
     Arvida::RDF::Triple triple;
+
     triple = Arvida::RDF::find_triple(ctx.model, _this0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:Translation3D"));
 
-    // 1. Check $this
     if (!triple.is_valid())
         return false;
     const Node _this = triple.subject;
 
-    // 2. Check blank _b0
     triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "vom:quantityValue"), Node());
     if (!triple.is_valid())
         return false;
     const Node _b0 = triple.object;
 
-    if (!Arvida::RDF::check_triple(ctx.model, _b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector3D")))
+    triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:Vector3D"));
+    if (!triple.is_valid())
         return false;
 
-    // maths:x
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:x"), Node());
     if (!triple.is_valid())
         return false;
@@ -149,7 +183,6 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Translation &value)
         value.setX(tmp_value);
     }
 
-    // maths:y
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:y"), Node());
     if (!triple.is_valid())
         return false;
@@ -160,7 +193,6 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Translation &value)
         value.setY(tmp_value);
     }
 
-    // maths:z
     triple = Arvida::RDF::find_triple(ctx.model, _b0, Curie(ctx.model.world(), "maths:z"), Node());
     if (!triple.is_valid())
         return false;
@@ -175,122 +207,75 @@ bool fromRDF(const Context &ctx, const NodeRef _this0, ::Translation &value)
 }
 
 template<>
-inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Pose &value)
+inline bool fromRDF(const Context &ctx, const NodeRef _this0, ::Pose &value)
 {
     using namespace Sord;
-    Node _b0 = Node::blank_id(ctx.model.world());
-    Node _b1 = Node::blank_id(ctx.model.world());
+    Arvida::RDF::Triple triple;
 
-    ctx.model.add_statement(_this, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:SpatialRelationship"));
-    ctx.model.add_statement(_b1, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:LeftHandedCartesianCoordinateSystem3D"));
-    ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:sourceCoordinateSystem"), _b1);
-    ctx.model.add_statement(_b0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:RightHandedCartesianCoordinateSystem2D"));
-    ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:targetCoordinateSystem"), _b0);
-    {
-        const std::string path_14 = ctx.path + "/transl";
-        Arvida::RDF::Context ctx_14(ctx, path_14);
-        Sord::URI node_14(ctx.model.world(), path_14);
-        ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:translation"), Arvida::RDF::toRDF(ctx_14, node_14, value.getTranslation()));
-    }
-    {
-        const std::string path_17 = ctx.path + "/rot";
-        Arvida::RDF::Context ctx_17(ctx, path_17);
-        Sord::URI node_17(ctx.model.world(), path_17);
-        ctx.model.add_statement(_this, Curie(ctx.model.world(), "spatial:rotation"), Arvida::RDF::toRDF(ctx_17, node_17, value.getRotation()));
-    }
-    return _this;
-}
-
-
-template <>
-bool fromRDF(const Context &ctx, const NodeRef _this0, ::Pose &value)
-{
-    using namespace Sord;
-
-    Arvida::RDF::Triple triple = Arvida::RDF::find_triple(ctx.model, _this0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:SpatialRelationship"));
-
-    // 1. Check $this
+    triple = Arvida::RDF::find_triple(ctx.model, _this0, Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "spatial:SpatialRelationship"));
     if (!triple.is_valid())
-    {
         return false;
-    }
     const Node _this = triple.subject;
 
-    // 2. Check blank _b1
     triple = Arvida::RDF::find_triple(ctx.model, Node(), Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:LeftHandedCartesianCoordinateSystem3D"));
     if (!triple.is_valid())
-    {
         return false;
-    }
     const Node _b1 = triple.subject;
 
-    // 3. Check blank _b1
-    if (!Arvida::RDF::check_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:sourceCoordinateSystem"), _b1))
-    {
+    triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:sourceCoordinateSystem"), _b1);
+    if (!triple.is_valid())
         return false;
-    }
 
-    // 4. Check blank _b0
     triple = Arvida::RDF::find_triple(ctx.model, Node(), Curie(ctx.model.world(), "rdf:type"), Curie(ctx.model.world(), "maths:RightHandedCartesianCoordinateSystem2D"));
     if (!triple.is_valid())
-    {
         return false;
-    }
-    Node _b0 = triple.subject;
+    const Node _b0 = triple.subject;
 
-    // 5. Check blank _b0
-    if (!Arvida::RDF::check_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:targetCoordinateSystem"), _b0))
-    {
+    triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:targetCoordinateSystem"), _b0);
+    if (!triple.is_valid())
         return false;
-    }
 
-    // 6. Check $that
     {
-        const std::string path_14 = ctx.path + "/transl";
-        Arvida::RDF::Context ctx_14(ctx, path_14);
-        Sord::URI node_14(ctx.model.world(), path_14);
-
-        triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:translation"), node_14);
+        const std::string path_27 = ctx.path + "/transl";
+        Arvida::RDF::Context ctx_27(ctx, path_27);
+        Sord::URI node_27(ctx.model.world(), path_27);
+        triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:translation"), node_27);
         if (!triple.is_valid())
             return false;
 
         {
             ::Translation tmp_value;
-            if (!Arvida::RDF::fromRDF(ctx, triple.object, tmp_value))
+            if (!Arvida::RDF::fromRDF(ctx_27, triple.object, tmp_value))
                 return false;
             value.setTranslation(tmp_value);
         }
     }
 
-    // 7. Check $that
     {
-        const std::string path_17 = ctx.path + "/rot";
-        Arvida::RDF::Context ctx_17(ctx, path_17);
-        Sord::URI node_17(ctx.model.world(), path_17);
-
-        triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:rotation"), node_17);
+        const std::string path_35 = ctx.path + "/rot";
+        Arvida::RDF::Context ctx_35(ctx, path_35);
+        Sord::URI node_35(ctx.model.world(), path_35);
+        triple = Arvida::RDF::find_triple(ctx.model, _this, Curie(ctx.model.world(), "spatial:rotation"), node_35);
         if (!triple.is_valid())
             return false;
 
         {
             ::Rotation tmp_value;
-            if (!Arvida::RDF::fromRDF(ctx, triple.object, tmp_value))
+            if (!Arvida::RDF::fromRDF(ctx_35, triple.object, tmp_value))
                 return false;
             value.setRotation(tmp_value);
         }
     }
-
     return true;
 }
 
 template<>
-inline NodeRef toRDF(const Context &ctx, NodeRef _this, const ::Device &value)
+inline bool fromRDF(const Context &ctx, const NodeRef _this0, ::Device &value)
 {
     using namespace Sord;
-    Node _b0 = Node::blank_id(ctx.model.world());
-    Node _b1 = Node::blank_id(ctx.model.world());
+    Arvida::RDF::Triple triple;
 
-    return _this;
+    return true;
 }
 
 } // namespace Arvida
